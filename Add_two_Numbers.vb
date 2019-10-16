1f4e0d8497fd01a612b4274fb5dcbdf10eb5155d
Faizan Ahmed <55249644+FaizanAhmed239@users.noreply.github.com>
2019-10-16 21:34:47 +0500

    Add files via upload

---
+++ Add_two_Numbers.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+    ' This program inputs two number from the user and sums them, providing the result.
+
+
+    Sub Main()
+
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim total As Integer
+
+        Console.WriteLine("Enter the Value of Num 1")
+        num1 = Console.ReadLine()
+
+        Console.WriteLine("Enter the Value of Num 2")
+        num2 = Console.ReadLine()
+
+        total = num1 + num2
+        Console.WriteLine("Total is:" & total)
+
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
