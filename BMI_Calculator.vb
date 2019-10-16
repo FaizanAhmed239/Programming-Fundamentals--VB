37709b1a7274f6cfd7819ddf9d9c5572e1e98ca9
Faizan Ahmed <55249644+FaizanAhmed239@users.noreply.github.com>
2019-10-16 21:36:01 +0500

    Add files via upload

---
+++ BMI_Calculator.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+
+    ' This program is A BMI application! It takes 30 inputs, of ID, Weight and Height and calculates the BMI using formula
+    ' Messages are also provided Throughout
+
+    Sub Main()
+        Dim Id As Integer
+        Dim weight As Integer
+        Dim height As Integer
+        Dim BMI As Integer
+
+        For x = 1 To 30
+            Console.WriteLine("Input ID")
+            Id = Console.ReadLine()
+
+            Console.WriteLine("Input weight(in kgs)")
+            weight = Console.ReadLine()
+
+            Console.WriteLine("Input height(in metres)")
+            height = Console.ReadLine()
+
+            BMI = weight / (height * height)
+            Console.WriteLine("ID:" & Id)
+
+            If BMI > 25 Then
+                Console.WriteLine("Over Weight" &vbNewLine)
+            ElseIf BMI < 25 And BMI > 19 Then
+                Console.WriteLine("Normal" &vbNewLine)
+            ElseIf BMI < 19 Then
+                Console.WriteLine("Under Weight" &vbNewLine)
+            End If
+
+
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
